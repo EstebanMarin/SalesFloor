9eefb25364c865edf3600f2e6aa49c797d8e498d
esteban marin <estebanmarin002@gmail.com>
2022-08-16 18:34:23 -0500

    Scala Client working

--- back_v1.0/s2salesfloor/src/main/scala/Main.scala
+++ back_v1.0/s2salesfloor/src/main/scala/Main.scala
@@ -2,28 +2,51 @@ import sttp.client3.quick._
 import sttp.client3.circe._
 
 import io.circe.generic.auto._
-
+import io.circe
+import sttp.client3.{Response, ResponseException}
+import sttp.client3.Identity
 
 case class GitHubResponse(total_count: Int, items: List[GitHubItem])
 case class GitHubItem(name: String, stargazers_count: Int)
 
-
+case class HackerRankById(id: Int)
+case class Item(
+    by: String,
+    descendants: Int,
+    id: Int,
+    Kids: List[Int],
+    time: Double,
+    title: String,
+    `type`: String,
+    url: String
+)
 
 object Main extends App {
-val query = "language:scala"
-val sort: Option[String] = Some("stars")
-val request = basicRequest
-  .get(uri"https://api.github.com/search/repositories?q=$query&sort=$sort")
-  .response(asJson[GitHubResponse])
-  
-val response = request.send(backend)
-response.body match {
-  case Left(error) => println(s"Error when executing request: $error")
-  case Right(data) =>
-    println(s"Found ${data.total_count} Scala projects.")
-    println(s"Showing ${data.items.size} with most stars:")
-    data.items.foreach { item =>
-      println(s"  ${item.name} (${item.stargazers_count})")
-    }
-}
+  type TopStoriesIds = List[HackerRankById]
+  val query = "language:scala"
+  val sort: Option[String] = Some("stars")
+  val getTopByID = basicRequest
+    .get(uri"https://hacker-news.firebaseio.com/v0/topstories.json")
+    .response(asJson[TopStoriesIds])
+
+  def getStory(hId: HackerRankById) = basicRequest
+    .get(uri"https://hacker-news.firebaseio.com/v0/item/${hId.id}.json")
+    .response(asJson[Item])
+
+  val getTopbyID = getTopByID.send(backend)
+  getTopbyID.body match {
+    case Left(error) => println(s"Error when executing request: $error")
+    case Right(data: TopStoriesIds) =>
+      println(s"[RESPONSE]  => \n ${data}")
+  }
+
+  def getItems(hId: HackerRankById) = getStory(hId).send(backend).body match {
+    case Left(error) => None
+    case Right(data: Item) =>
+      println(s"[RESPONSE]  => \n ${data}")
+      Some(data)
+  }
+  def mapOverID(data: TopStoriesIds): List[Option[Item]] =
+    data.map(getItems).filter(_.nonEmpty)
+
 }
